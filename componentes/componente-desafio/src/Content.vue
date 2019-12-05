65e83bd60257be32ce9c7001abf203e430e28a40
Thiago Maciel <thiagomrvieira@gmail.com>
2019-12-05 22:47:05 +0000

    Hora de praticar: Resolução

---
+++ componentes/componente-desafio/src/Content.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-content>
+        <carousel />
+    </v-content>
+
+</template>
+<script>
+    import carousel from './Carousel.vue'
+    export default {
+        components: { carousel: carousel}
+    }
+
+    
+</script>
+
+<style>
+
+</style>
