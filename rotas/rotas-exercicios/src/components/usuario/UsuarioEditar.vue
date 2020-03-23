7be088595c3fe126963924777bc501d5342b1256
Thiago Maciel <thiagomrvieira@gmail.com>
2020-03-23 17:48:28 +0000

    Navegando para rotas aninhadas

--- rotas/rotas-exercicios/src/components/usuario/UsuarioEditar.vue
+++ rotas/rotas-exercicios/src/components/usuario/UsuarioEditar.vue
@@ -1,12 +1,14 @@
 <template>
     <div class="usuario-editar">
         <h3>Editar usuário</h3>
+        <p><strong>Código do usuário: </strong>{{id}}</p>
+
     </div>
 </template>
 
 <script>
 export default {
-
+    props: ['id'],
 }
 </script>
 
