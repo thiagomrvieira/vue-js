c5999c13cc7f199ac516a43b735344c51fb33b2d
Thiago Maciel <thiagomrvieira@gmail.com>
2020-03-23 17:57:50 +0000

    Tornando o router-link mais dinâmico

--- rotas/rotas-exercicios/src/components/template/Menu.vue
+++ rotas/rotas-exercicios/src/components/template/Menu.vue
@@ -2,7 +2,7 @@
     <nav class="menu"> 
         <ul>
             <router-link to="/" tag="li" exact-active-class="active" > <a> Início </a></router-link>
-            <router-link to="/usuario" tag="li" exact-active-class="active"> <a> Usuário </a></router-link>
+            <router-link to="/usuario" tag="li" active-class="active"> <a> Usuário </a></router-link>
 
         </ul>
     </nav>
--- rotas/rotas-exercicios/src/components/usuario/UsuarioDetalhe.vue
+++ rotas/rotas-exercicios/src/components/usuario/UsuarioDetalhe.vue
@@ -2,7 +2,7 @@
     <div class="usuario-detalhe">
         <h3>Usuário detalhe</h3>
         <p><strong>Código do usuário: </strong>{{id}}</p>
-
+        <router-link tag="button" primario :to="`/usuario/${id}/editar`">Editar</router-link>
     </div>
 </template>
 
