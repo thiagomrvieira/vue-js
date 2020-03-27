c4da7dafa08bd03483eca0079559c9b93746bc90
Thiago Maciel <thiagomrvieira@gmail.com>
2020-03-27 13:31:20 +0000

    Passando o fragmento hash

--- rotas/rotas-exercicios/src/components/usuario/UsuarioDetalhe.vue
+++ rotas/rotas-exercicios/src/components/usuario/UsuarioDetalhe.vue
@@ -4,7 +4,9 @@
         <p><strong>Código do usuário: </strong>{{id}}</p>
         <!-- <router-link tag="button" primario :to="`/usuario/${id}/editar`">Editar</router-link> -->
         <router-link tag="button" primario 
-            :to="{name: 'editarUsuario', params:{id}, query:{ completo: false, idioma: 'Pt-br'}}">
+            :to="{ name: 'editarUsuario', params:{id}, 
+                   query:{ completo: false, idioma: 'Pt-br'},
+                   hash:'#rodape'}">
             Editar</router-link>
     </div>
 </template>
--- rotas/rotas-exercicios/src/components/usuario/UsuarioEditar.vue
+++ rotas/rotas-exercicios/src/components/usuario/UsuarioEditar.vue
@@ -4,6 +4,9 @@
         <p><strong>Código do usuário: </strong>{{id}}</p>
         <p><strong>Completo:</strong> {{$route.query.completo ? 'Sim' : 'Não'}}</p>
         <p><strong>Idioma:</strong> {{$route.query.idioma}}</p>
+        <div id="rodape">
+            <h3>Curso vue</h3>
+        </div>
     </div>
 </template>
 
@@ -14,5 +17,7 @@ export default {
 </script>
 
 <style>
-
+    #rodape{
+        margin-top: 1000px;
+    }
 </style>
