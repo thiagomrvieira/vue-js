133313bbae1b16b06906d90f33d57afa6ae031a1
Thiago Maciel <thiagomrvieira@gmail.com>
2020-03-27 13:21:15 +0000

    Redirecionamento

--- rotas/rotas-exercicios/src/router.js
+++ rotas/rotas-exercicios/src/router.js
@@ -35,8 +35,11 @@ export default new Router({
             { path: ':id', component: UsuarioDetalhe, props: true},
             { path: ':id/editar', component: UsuarioEditar, props: true,
               name: 'editarUsuario'
-            },
-        
+            }
         ]
-    }]
+    },{
+        path:'/redirecionar',
+        redirect: '/usuario'
+    }
+]
 })
