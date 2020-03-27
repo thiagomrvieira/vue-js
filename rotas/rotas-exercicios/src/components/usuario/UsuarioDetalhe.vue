4586c6d192a9e554ab9b775d23a22ad34e4799e7
Thiago Maciel <thiagomrvieira@gmail.com>
2020-03-27 12:21:57 +0000

    Usando parâmetros da rota

--- rotas/rotas-exercicios/src/components/usuario/UsuarioDetalhe.vue
+++ rotas/rotas-exercicios/src/components/usuario/UsuarioDetalhe.vue
@@ -3,7 +3,9 @@
         <h3>Usuário detalhe</h3>
         <p><strong>Código do usuário: </strong>{{id}}</p>
         <!-- <router-link tag="button" primario :to="`/usuario/${id}/editar`">Editar</router-link> -->
-        <router-link tag="button" primario :to="{name: 'editarUsuario', params:{id}}">Editar</router-link>
+        <router-link tag="button" primario 
+            :to="{name: 'editarUsuario', params:{id}, query:{ completo: false, idioma: 'Pt-br'}}">
+            Editar</router-link>
     </div>
 </template>
 
--- rotas/rotas-exercicios/src/components/usuario/UsuarioEditar.vue
+++ rotas/rotas-exercicios/src/components/usuario/UsuarioEditar.vue
@@ -2,7 +2,8 @@
     <div class="usuario-editar">
         <h3>Editar usuário</h3>
         <p><strong>Código do usuário: </strong>{{id}}</p>
-
+        <p><strong>Completo:</strong> {{$route.query.completo ? 'Sim' : 'Não'}}</p>
+        <p><strong>Idioma:</strong> {{$route.query.idioma}}</p>
     </div>
 </template>
 
