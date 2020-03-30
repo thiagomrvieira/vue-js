e514188ed30a7f04e406cea227189df8915a0e6b
Thiago Maciel <thiagomrvieira@gmail.com>
2020-03-30 15:21:27 +0100

    Protegendo rotas

--- rotas/rotas-exercicios/src/components/usuario/UsuarioEditar.vue
+++ rotas/rotas-exercicios/src/components/usuario/UsuarioEditar.vue
@@ -4,6 +4,8 @@
         <p><strong>Código do usuário: </strong>{{id}}</p>
         <p><strong>Completo:</strong> {{$route.query.completo ? 'Sim' : 'Não'}}</p>
         <p><strong>Idioma:</strong> {{$route.query.idioma}}</p>
+        <hr>
+        <button primario> Confirmar</button>
         <div id="rodape">
             <h3>Curso vue</h3>
         </div>
